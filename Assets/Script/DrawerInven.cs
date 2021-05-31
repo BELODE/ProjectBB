a52cd92158877d2b8b079cc22c387828af6802e0
DESKTOP-CCKFT7H\xodlq <xodlqxodlq@gmail.com>
2021-06-01 00:39:02 +0900

    [Client] 누락스크립트

---
+++ Assets/Script/DrawerInven.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerInven : MonoBehaviour
+{
+    public List<int> itemsCode = new List<int>();
+
+    private MaterialManager mManager;
+    private PlayerMove player;
+    private ItemSpawner itemS;
+
+    void Start()
+    {
+        mManager = GameObject.Find("materialManager").GetComponent<MaterialManager>();
+        player = GameObject.Find("Player").GetComponent<PlayerMove>();
+        itemS = GameObject.Find("ItemSpawner").GetComponent<ItemSpawner>();
+        int rand = Random.Range(1, 5);
+        for (int i = 0; i < rand; i++)
+        {
+            itemsCode.Add(Random.Range(0, GameObject.Find("ItemSpawner").GetComponent<ItemSpawner>().items.Length));
+        }
+    }
+
+    void Update()
+    {
+        MaterialChange();
+    }
+
+    private void MaterialChange()
+    {
+        if (player.target == gameObject)
+        {
+            transform.GetComponent<SpriteRenderer>().material = mManager.change;
+        }
+        else
+        {
+            transform.GetComponent<SpriteRenderer>().material = mManager.origin;
+        }
+    }
+
+    public void ItemSpawn()
+    {
+        if (itemsCode.Count > 0)
+        {
+            int rand = Random.Range(0, itemsCode.Count);
+            GameObject gameObject = Instantiate(itemS.items[itemsCode[rand]], GameObject.Find("Player").GetComponent<PlayerMove>().target.transform.position, Quaternion.identity) as GameObject;
+            itemsCode.RemoveAt(rand);
+        }
+    }
+
+    public void ItemIn(int code)
+    {
+        itemsCode.Add(code);
+    }
+
+    public bool GetOpen()
+    {
+        return transform.GetComponent<Animator>().GetBool("Open");
+    }
+
+    public void SetOpen(bool open)
+    {
+        transform.GetComponent<Animator>().SetBool("Open", open);
+    }
+}
