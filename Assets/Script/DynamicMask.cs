8162ebbaa705693b9e6c171b8023a5a74403a1aa
RNC99 <son990823@gmail.com>
2021-06-07 04:54:53 +0900

    Client/ 플레이어 실루엣 쉐이더 추가 및 전시관 복도 리소스 재배치 후 그림자 설정

---
+++ Assets/Script/DynamicMask.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicMask : MonoBehaviour
+{
+    SpriteRenderer _sr;
+    SpriteMask _sm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+        _sm = GetComponent<SpriteMask>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _sm.sprite = _sr.sprite;
+    }
+}
