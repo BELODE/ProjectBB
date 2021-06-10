dccf04e51a79bfa5359aa8421d25d7772b022597
RNC99 <son990823@gmail.com>
2021-06-10 12:56:09 +0900

    Client/옵션 창 요소 일부 업데이트

---
+++ Assets/OptionPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionPanel : MonoBehaviour
+{
+    public Resolution[] resolutions;
+    public Dropdown resolutionDropDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "*" + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropDown.AddOptions(options);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
